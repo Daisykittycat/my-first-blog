32272bb9daaa5b8d83e843650cb723d8e6ab82db
Gemma Lewis <gemlouise177@gmail.com>
2018-09-15 15:49:11 +0100

    My Django Girls app, first commit

---
+++ mysite/python_intro.py
@@ -0,0 +1,41 @@
+print('Hello, Django Girls')
+print("Welcome to my Python page")
+
+lottery = [12,17,7, 100]
+print('You have the winning ticket ')
+
+print (lottery)
+
+if 3 > 2:
+    print('It works!')
+else:
+    print('It has broken')
+
+def hione(name):
+
+    if name == 'Ola':
+        print('Hey Ola!')
+    elif name == 'Gemma':
+        print('Hey Sonja!')
+    else:
+        print('Hey anonymous!')
+
+    
+
+hione('Ola')
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+hi("Rachel")
+
+
+girls = ['rachael', 'cai']
+
+for name in girls:
+    hi(name)
+    print('next girl')
+
+
+for i in range (1,7)
+    print(i)
